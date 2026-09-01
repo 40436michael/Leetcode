88ce388637a07a2f0428197a5b77f9609cd685f2
加乘 <93032489+40436michael@users.noreply.github.com>
2026-09-01 23:01:35 +0800

    [49] Group Anagrams 之後再練

--- solutions/49.group-anagrams.cpp
+++ solutions/49.group-anagrams.cpp
@@ -8,27 +8,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> table;
+        unordered_map<string,vector<string>> umap;
         vector<vector<string>> ans;
-        string tmp;
-        string s;
-        ans.push_back({strs[0]});
-        for(int i=1;i<strs.size();i++){
-            bool found_anagrams=false; 
-            s=strs[i];
-            sort(s.begin(),s.end());
-            for(int j=0;j<ans.size();j++){
-                tmp=ans[j][0];
-                sort(tmp.begin(),tmp.end());
-                if(s==tmp){
-                    ans[j].push_back(strs[i]);
-                    found_anagrams=true;
-                    break;
-                }                
-            }
-            if(!found_anagrams){
-                ans.push_back({strs[i]});
-            }
+        for(const auto& i:strs){
+            string key=i;
+            sort(key.begin(),key.end());
+            umap[key].push_back(i);
+        }
+        for(const auto& i:umap){
+            ans.push_back(i.second);
         }
         return ans;        
     }
