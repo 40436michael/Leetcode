0880dcdad11510166642058d5d0478621720c0ed
加乘 <93032489+40436michael@users.noreply.github.com>
2026-09-14 14:12:25 +0800

    [836] Rectangle Overlap 卡了一下

---
+++ solutions/836.rectangle-overlap.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=836 lang=cpp
+ *
+ * [836] Rectangle Overlap
+ */
+// @status: unfamiliar
+// @lc code=start
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        if(rec1[2]<=rec2[0]||rec1[0]>=rec2[2]||rec1[3]<=rec2[1]||rec1[1]>=rec2[3])
+            return false;
+        else 
+            return true;
+    }
+};
+// @lc code=end
+
